5dc9b4bc3912bcaafabf074bbfde349585f36317
acecavaliers <95069460+acecavaliers@users.noreply.github.com>
2024-06-28 16:48:03 +0800

    task

--- database/migrations/2024_06_25_105754_create_tasks_table.php
+++ database/migrations/2024_06_25_105754_create_tasks_table.php
@@ -13,7 +13,7 @@ return new class extends Migration
     {
         Schema::create('tasks', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
+            $table->string('name')->nullable();
             $table->string('status');
             $table->tinyInteger('is_active');
             $table->timestamps();
